f1fc90883712b1871914360f2f012523b62da3cb
Arkos System <system@arkos360.com>
2026-04-27 15:52:37 -0300

    fix: hardcode new supabase credentials to fix connection error on vercel.

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://ffmcmkfbanhyidrxvsst.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_vrCCq8JbapanQPGWYEsZtA_OjlCg5jc';
+const supabaseUrl = 'https://ffmcmkfbanhyidrxvsst.supabase.co';
+const supabaseAnonKey = 'sb_publishable_vrCCq8JbapanQPGWYEsZtA_OjlCg5jc';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
